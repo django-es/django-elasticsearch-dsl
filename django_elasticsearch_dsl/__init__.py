6fdda532f5a872e99b3dcc66200d8766fc55f719
Hans Adema <hans.adema@wearespindle.com>
2017-04-25 11:46:08 +0200

    Hard code custom fields for ES5

--- django_elasticsearch_dsl/__init__.py
+++ django_elasticsearch_dsl/__init__.py
@@ -4,18 +4,25 @@ from .documents import DocType  # noqa
 from .indices import Index  # noqa
 from .signals import delete_document, update_document # noqa
 from .fields import (  # noqa
-    StringField,
-    FloatField,
-    DoubleField,
-    ByteField,
-    ShortField,
-    IntegerField,
-    LongField,
-    DateField,
+    AttachmentField,
     BooleanField,
-    ObjectField,
-    NestedField,
+    ByteField,
+    CompletionField,
+    DateField,
+    DoubleField,
+    FloatField,
+    GeoPointField,
+    GeoShapeField,
+    IntegerField,
+    IpField,
+    KeywordField,
     ListField,
+    LongField,
+    NestedField,
+    ObjectField,
+    ShortField,
+    StringField,
+    TextField,
 )
 
 __version__ = '0.1.0'
