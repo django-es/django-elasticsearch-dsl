c5fbb4f068ef9ac4d6055024f8dc3f08ceedd341
sabricot <julien.sabre@epitech.eu>
2017-05-04 21:44:29 +0200

    Add django 1.11 support

--- runtests.py
+++ runtests.py
@@ -15,7 +15,6 @@ try:
                     "ENGINE": "django.db.backends.sqlite3",
                 }
             },
-            ROOT_URLCONF="django_elasticsearch_dsl.urls",
             INSTALLED_APPS=[
                 "django.contrib.auth",
                 "django.contrib.contenttypes",
