af9eceebddb452f43cc59b7fa03758c42b45c708
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-06-26 02:40:51 +0200

    replace simple model signals with easier to customise signal processors

--- django_elasticsearch_dsl/apps.py
+++ django_elasticsearch_dsl/apps.py
@@ -1,15 +1,28 @@
 from django.apps import AppConfig
-from elasticsearch_dsl.connections import connections
 from django.conf import settings
+
+from elasticsearch_dsl.connections import connections
+
+from .utils import import_class
 
 
 class DEDConfig(AppConfig):
     name = 'django_elasticsearch_dsl'
     verbose_name = "Django elasticsearch-dsl"
+    signal_processor = None
 
     def ready(self):
         self.module.autodiscover()
         connections.configure(**settings.ELASTICSEARCH_DSL)
+        # Setup the signal processor.
+        if not self.signal_processor:
+            signal_processor_path = getattr(
+                settings,
+                'ELASTICSEARCH_DSL_SIGNAL_PROCESSOR',
+                'django_elasticsearch_dsl.signals.RealTimeSignalProcessor'
+            )
+            signal_processor_class = import_class(signal_processor_path)
+            self.signal_processor = signal_processor_class(connections)
 
     @classmethod
     def autosync_enabled(cls):
