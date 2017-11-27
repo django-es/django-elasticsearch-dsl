17d4bd444663d2bcb02bd48b3ba3612ea36c5dcd
sabricot <julien.sabre@epitech.eu>
2017-11-27 22:08:39 +0100

    Bump version: v0.4.1 → 0.4.2

--- django_elasticsearch_dsl/__init__.py
+++ django_elasticsearch_dsl/__init__.py
@@ -4,7 +4,7 @@ from .documents import DocType  # noqa
 from .indices import Index  # noqa
 from .fields import *  # noqa
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 
 def autodiscover():
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '0.4.1'
+version = '0.4.2'
 
 if sys.argv[-1] == 'publish':
     try:
