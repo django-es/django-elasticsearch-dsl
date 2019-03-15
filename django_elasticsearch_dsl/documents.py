fafdeeb281792b6d9b684e6695d292c40dda9f3e
Danny Aziz <danny@Dannys-MacBook-Pro.local>
2019-03-15 19:59:29 +0000

    remove iterating count

--- django_elasticsearch_dsl/documents.py
+++ django_elasticsearch_dsl/documents.py
@@ -207,10 +207,7 @@ class DocType(DSLDocType):
             paginator = Paginator(
                 object_list, self._doc_type.queryset_pagination
             )
-            number_of_pages = len(paginator.page_range)
-            current_page = 0
             for page in paginator.page_range:
-                current_page += 1
                 for object_instance in paginator.page(page).object_list:
                     yield self._prepare_action(object_instance, action)
         else:
