3b690090dffa28812ec7536d24559024dc4db288
Martin J. Laubach <mjl@laubach.at>
2019-10-25 15:08:38 +0200

    Correctly clean up after test run (delete indices with the right name).

--- django_elasticsearch_dsl/test/testcases.py
+++ django_elasticsearch_dsl/test/testcases.py
@@ -19,11 +19,11 @@ class ESTestCase(object):
     def tearDown(self):
         pattern = re.compile(self._index_suffixe + '$')
 
-        for doc in registry.get_documents():
-            doc._index._name = pattern.sub('', doc._index._name)
-
         for index in registry.get_indices():
             index.delete(ignore=[404, 400])
             index._name = pattern.sub('', index._name)
+
+        for doc in registry.get_documents():
+            doc._index._name = pattern.sub('', doc._index._name)
 
         super(ESTestCase, self).tearDown()
