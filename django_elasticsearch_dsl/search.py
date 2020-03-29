0365cc12f9b99b78e6f2723f8d43bfa59c48834c
Andrey Laguta <13280256+laevilgenius@users.noreply.github.com>
2020-03-29 04:07:59 +0300

    Remove _get_queryset

--- django_elasticsearch_dsl/search.py
+++ django_elasticsearch_dsl/search.py
@@ -1,5 +1,4 @@
 from django.db.models import Case, When
-from django.shortcuts import _get_queryset
 
 from elasticsearch_dsl import Search as DSLSearch
 
@@ -14,19 +13,16 @@ class Search(DSLSearch):
         s._model = self._model
         return s
 
-    def filter_queryset(self, klass, keep_search_order=True):
+    def filter_queryset(self, queryset, keep_search_order=True):
         """
         Filter an existing django queryset using the elasticsearch result.
         It costs a query to the sql db.
-        klass may be a Model, Manager, or QuerySet object.
         """
-
-        qs = _get_queryset(klass)
         s = self
-        if s._model is not qs.model:
+        if s._model is not queryset.model:
             raise TypeError(
                 'Unexpected queryset model '
-                '(should be: %s, got: %s)' % (s._model, qs.model)
+                '(should be: %s, got: %s)' % (s._model, queryset.model)
             )
 
         # Do not query again if the es result is already cached
@@ -36,19 +32,19 @@ class Search(DSLSearch):
             s = s.execute()
 
         pks = [result.meta.id for result in s]
-        qs = qs.filter(pk__in=pks)
+        queryset = queryset.filter(pk__in=pks)
 
         if keep_search_order:
             preserved_order = Case(
                 *[When(pk=pk, then=pos) for pos, pk in enumerate(pks)]
             )
-            qs = qs.order_by(preserved_order)
+            queryset = queryset.order_by(preserved_order)
 
-        return qs
+        return queryset
 
     def to_queryset(self, keep_order=True):
         """
         Return a django queryset from the elasticsearch result.
         It costs a query to the sql db.
         """
-        return self.filter_queryset(self._model, keep_order)
+        return self.filter_queryset(self._model._default_manager, keep_order)
