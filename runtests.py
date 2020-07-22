8498721b5d4d052082a0a89b689da3d0d18e7b93
Artur Barseghyan <artur.barseghyan@target-holding.nl>
2020-07-22 16:36:35 +0200

    Clean up

--- runtests.py
+++ runtests.py
@@ -63,7 +63,6 @@ def make_parser():
 
 def run_tests(*test_args):
     args, test_args = make_parser().parse_known_args(test_args)
-
     if args.elasticsearch:
         os.environ.setdefault('ELASTICSEARCH_URL', args.elasticsearch)
 
