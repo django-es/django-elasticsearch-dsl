452a2ac295c0e1537310ebe06657abef1966b7ca
nimac <michael12sco@gmail.com>
2021-05-05 15:21:16 +0000

    fix

--- django_elasticsearch_dsl/signals.py
+++ django_elasticsearch_dsl/signals.py
@@ -6,7 +6,9 @@ cause things to index.
 
 from __future__ import absolute_import
 
-from django.db import models
+from celery import shared_task
+from django.apps import apps
+from django.db import models, transaction
 
 from .registries import registry
 
@@ -100,3 +102,63 @@ class RealTimeSignalProcessor(BaseSignalProcessor):
         models.signals.post_delete.disconnect(self.handle_delete)
         models.signals.m2m_changed.disconnect(self.handle_m2m_changed)
         models.signals.pre_delete.disconnect(self.handle_pre_delete)
+
+
+@shared_task
+def handle_save(pk, app_label, model_name):
+    sender = apps.get_model(app_label, model_name)
+    instance = sender.objects.get(pk=pk)
+    registry.update(instance)
+    registry.update_related(instance)
+
+
+@shared_task
+def handle_pre_delete(pk, app_label, model_name):
+    sender = apps.get_model(app_label, model_name)
+    instance = sender.objects.get(pk=pk)
+    registry.delete_related(instance)
+
+
+@shared_task
+def handle_delete(pk, app_label, model_name):
+    sender = apps.get_model(app_label, model_name)
+    instance = sender.objects.get(pk=pk)
+    registry.delete(instance, raise_on_error=False)
+
+
+class CelerySignalProcessor(RealTimeSignalProcessor):
+    """Celery signal processor.
+    Allows automatic updates on the index as delayed background tasks using
+    Celery.
+    NB: We cannot process deletes as background tasks.
+    By the time the Celery worker would pick up the delete job, the
+    model instance would already deleted. We can get around this by
+    setting Celery to use `pickle` and sending the object to the worker,
+    but using `pickle` opens the application up to security concerns.
+    """
+
+    def handle_save(self, sender, instance, **kwargs):
+        """Handle save.
+        Given an individual model instance, update the object in the index.
+        Update the related objects either.
+        """
+        app_label = instance._meta.app_label
+        model_name = instance._meta.model_name
+        transaction.on_commit(lambda: handle_save.delay(instance.pk, app_label, model_name))
+
+    def handle_pre_delete(self, sender, instance, **kwargs):
+        """Handle removing of instance object from related models instance.
+        We need to do this before the real delete otherwise the relation
+        doesn't exists anymore and we can't get the related models instance.
+        """
+        app_label = instance._meta.app_label
+        model_name = instance._meta.model_name
+        handle_pre_delete.delay(instance.pk, app_label, model_name)
+
+    def handle_delete(self, sender, instance, **kwargs):
+        """Handle delete.
+        Given an individual model instance, delete the object from index.
+        """
+        app_label = instance._meta.app_label
+        model_name = instance._meta.model_name
+        handle_delete.delay(instance.pk, app_label, model_name)
