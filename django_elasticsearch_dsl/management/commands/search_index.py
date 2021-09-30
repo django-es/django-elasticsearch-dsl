e86580a8ad727b92609b0088d4e98d4e93aafcfa
oehrlein <timothyoehrlein@gmail.com>
2021-09-30 01:42:18 -0500

    Updated 'indexes' to 'indices' for verbiage consistency

--- django_elasticsearch_dsl/management/commands/search_index.py
+++ django_elasticsearch_dsl/management/commands/search_index.py
@@ -146,7 +146,7 @@ class Command(BaseCommand):
         if not options['force']:
             response = input(
                 "Are you sure you want to delete "
-                "the '{}' indexes? [y/N]: ".format(", ".join(index_names)))
+                "the '{}' indices? [y/N]: ".format(", ".join(index_names)))
             if response.lower() != 'y':
                 self.stdout.write('Aborted')
                 return False
