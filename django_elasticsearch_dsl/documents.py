ce057156354ba63b823cc5eb9fb01dd0125704a5
oehrlein <timothyoehrlein@gmail.com>
2021-10-06 03:24:09 -0500

    Added _matches() to DocType for alias pattern matching

--- django_elasticsearch_dsl/documents.py
+++ django_elasticsearch_dsl/documents.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from collections import deque
+from fnmatch import fnmatch
 from functools import partial
 
 from django import VERSION as DJANGO_VERSION
@@ -65,6 +66,18 @@ class DocType(DSLDocument):
 
     def __hash__(self):
         return id(self)
+
+    @classmethod
+    def _matches(cls, hit):
+        """
+        Determine which index or indices in a pattern to be used in a hit.
+        Overrides DSLDocument _matches function to match indices in a pattern,
+        which is needed in case of using aliases. This is needed as the
+        document class will not be used to deserialize the documents. The
+        documents will have the index set to the concrete index, whereas the
+        class refers to the alias.
+        """
+        return fnmatch(hit.get("_index", ""), cls._index._name + "*")
 
     @classmethod
     def search(cls, using=None, index=None):
