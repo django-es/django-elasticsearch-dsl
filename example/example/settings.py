456efc1ec7d62f26031585c444dd85f5b1735d07
Safwan Rahman <safwan.rahman15@gmail.com>
2022-11-01 12:19:14 +0600

    [Fix] Fixing Example app (#428)
    * [Fix] Fixing Example app
    
    * [Fix] Removing docker

--- example/example/settings.py
+++ example/example/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autofixture',
 
     'django_elasticsearch_dsl',
 
--- example/example/urls.py
+++ example/example/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-import autofixture
-from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
-
-
-autofixture.autodiscover()
