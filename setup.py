66dbe926b9686d86e58d209532b812a9e34e6fc4
oehrlein <timothyoehrlein@gmail.com>
2023-09-13 23:31:56 -0500

    Updated install requirements in setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'elasticsearch-dsl>=7.2.0,<8.0.0',
+        'elasticsearch-dsl>=8.9.0,<9.0.0',
         'six',
     ],
     license="Apache Software License 2.0",
